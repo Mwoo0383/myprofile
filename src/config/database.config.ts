cca076816331f9bb7557ee3c362f460a18a34842
Minwoo <cap019@naver.com>
2026-01-27 22:06:22 +0900

    MySQL 연결

---
+++ src/config/database.config.ts
@@ -0,0 +1,20 @@
+// src/config/database.config.ts
+import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
+  imports: [],
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService) => ({
+    type: 'mysql',
+    host: configService.get('DB_HOST'),
+    port: Number(configService.get('DB_PORT')),
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_NAME'),
+
+    autoLoadEntities: true,
+    synchronize: false,
+    logging: true,
+  }),
+};

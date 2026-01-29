a02ad8c3aa24418f74db48e25987738e1db324f4
Minwoo <cap019@naver.com>
2026-01-29 20:10:38 +0900

    getProfile() 타입을 profileResponseDto로 명시. 반환

--- src/modules/profile/profile.service.ts
+++ src/modules/profile/profile.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Profile } from './profile.entity';
+import { ProfileResponseDto } from './dto/profile-response.dto';
 
 @Injectable()
 export class ProfileService {
@@ -10,7 +11,7 @@ export class ProfileService {
     private readonly profileRepository: Repository<Profile>,
   ) {}
 
-  async getProfile() {
+  async getProfile(): Promise<ProfileResponseDto> {
     const profiles = await this.profileRepository.find({
       relations: ['user'],
     });
@@ -18,7 +19,13 @@ export class ProfileService {
     if (profiles.length === 0) {
       throw new NotFoundException('Profile not found');
     }
+    const profile = profiles[0];
   
-    return profiles[0];
+    return {
+      name : profile.name,
+      bio : profile.bio,
+      phone : profile.phone,
+      email : profile.user.email,
+    }
   }
 }

1dc5a6f668022f4c512b1af70b561b581d9a34d1
Minwoo <cap019@naver.com>
2026-02-04 20:19:21 +0900

    Next.js CORS 설정 - port 3000

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,10 @@ import "reflect-metadata";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();

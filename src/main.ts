c04f08347d9114628070dffbd8736dfb14542a85
Minwoo <cap019@naver.com>
2026-02-12 18:46:19 +0900

    배포용 CORS 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,10 @@ import "reflect-metadata";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://myprofile-frontend.vercel.app/',
+    ],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3001);
